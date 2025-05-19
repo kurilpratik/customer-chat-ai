36d2930b24293f89a13564e126b4364aa5ad0fb8
Pratik Kuril <pratikkuril16@gmail.com>
2025-05-20 02:32:02 +0530

    Add inbox and dummy data

--- app/_common/Inbox.jsx
+++ app/_common/Inbox.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { messages } from "../data/data";
+import { ChevronDownIcon } from "lucide-react";
 
 const Inbox = () => {
-  return <div className="h-screen flex-1">Inbox</div>;
+  return (
+    <div className="h-screen flex-1 px-4 py-2">
+      <h2 className="mb-8 text-lg font-bold">Your inbox</h2>
+      <div className="mb-4 flex justify-between font-semibold">
+        <div className="flex items-center">
+          <span className="pr-2">5</span> Open{" "}
+          <ChevronDownIcon height={20} />{" "}
+        </div>
+        <div className="flex items-center">
+          Waiting Longest <ChevronDownIcon height={20} />{" "}
+        </div>
+      </div>
+
+      {/* MESSAGES  */}
+      <ul className="flex flex-col gap-1">
+        {messages.map((message) => (
+          // MESSAGE
+          <li
+            key={message.id}
+            className={`ml-[-0.8rem] flex cursor-pointer items-center gap-2 rounded-xl px-4 py-2 hover:bg-indigo-100 ${message.read ? "opacity-40" : ""}`}
+          >
+            <div
+              className={`flex h-10 w-10 items-center justify-center rounded-full font-bold text-white ${message.color}`}
+            >
+              {message.sender
+                ? message.sender.charAt(0)
+                : message.source.charAt(0)}
+            </div>
+            <div>
+              <h3 className="text-md">
+                {message.sender ? message.sender + " - " : message.sender}
+                {message.source}
+              </h3>
+              <p>
+                {message.content.length > 24
+                  ? `${message.content.slice(0, 24)}...`
+                  : message.content}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Inbox;
---
+++ app/data/data.js
@@ -0,0 +1,49 @@
+export const messages = [
+  {
+    id: 1,
+    source: "Github",
+    content: "Hello, I have a question about the project",
+    sender: "John",
+    time: "25m",
+    read: false,
+    color: "bg-indigo-400",
+  },
+  {
+    id: 2,
+    source: "Hike",
+    content: "Hi there, how are you? I wanted to ask you about the project",
+    sender: "Ivan",
+    time: "32m",
+    read: false,
+    color: "bg-red-400",
+  },
+  {
+    id: 3,
+    source: "Lead from New York",
+    content: "Good morning, I'm looking for a new apartment",
+    sender: "",
+    time: "35m",
+    read: false,
+    color: "bg-blue-400",
+  },
+  {
+    id: 4,
+    source: "Booking API Problems",
+    content: "Hi, I wanted to submit a complaint about the booking",
+    sender: "",
+    time: "45m",
+    read: true,
+    color: "bg-pink-400",
+  },
+  {
+    id: 5,
+    source: "Exemptary Bank",
+    content: "Hello, how are you? We would like to discuss the loan",
+    sender: "Miracle",
+    time: "49m",
+    read: true,
+    color: "bg-indigo-400",
+  },
+];
+
+export { messages };
