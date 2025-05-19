02b2b76adf44abc6d2ca0494157b15109f60c340
Pratik Kuril <pratikkuril16@gmail.com>
2025-05-20 04:00:18 +0530

    Add basic chat UI

--- app/_common/Chat.jsx
+++ app/_common/Chat.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { chat } from "../data/data";
+import { Button } from "@/components/ui/button";
+import { PanelBottomClose, Ellipsis, MoonStar } from "lucide-react";
 
 const Chat = () => {
-  return <div className="h-screen w-full flex-3">Chat</div>;
+  return (
+    <div className="mx-4 h-screen w-full flex-3 py-2">
+      <nav className="mb-8 flex justify-between">
+        <h3 className="text-lg font-bold">Luis Easton</h3>
+        <div className="flex gap-2">
+          <Button variant="secondary">
+            <Ellipsis />
+          </Button>
+          <Button variant="secondary">
+            <MoonStar />
+          </Button>
+          <Button>
+            <PanelBottomClose /> Close
+          </Button>
+        </div>
+      </nav>
+
+      {/* CHAT */}
+      <div className="flex flex-col gap-4 px-6">
+        {chat.map((message) => (
+          <div
+            key={message.id}
+            className={`flex gap-4 ${message.id % 2 == 0 ? "flex-row-reverse" : ""}`}
+          >
+            <div
+              className={`flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full font-bold text-white ${message.color}`}
+            >
+              {message.sender.charAt(0)}
+            </div>
+            <div
+              className={`rounded-xl p-4 ${message.id % 2 == 0 ? "bg-indigo-200" : "bg-gray-200"}`}
+            >
+              <p className="pb-2 font-medium">{message.content}</p>
+              <p className="text-sm">{message.time}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- app/data/data.js
+++ app/data/data.js
@@ -1,4 +1,4 @@
-export const messages = [
+const messages = [
   {
     id: 1,
     source: "Github",
@@ -12,7 +12,7 @@ export const messages = [
     id: 2,
     source: "Hike",
     content: "Hi there, how are you? I wanted to ask you about the project",
-    sender: "Ivan",
+    sender: "Ivan Rakitic",
     time: "32m",
     read: false,
     color: "bg-red-400",
@@ -39,11 +39,29 @@ export const messages = [
     id: 5,
     source: "Exemptary Bank",
     content: "Hello, how are you? We would like to discuss the loan",
-    sender: "Miracle",
+    sender: "Miracle Morning",
     time: "49m",
     read: true,
     color: "bg-indigo-400",
   },
 ];
 
-export { messages };
+const chat = [
+  {
+    id: 1,
+    sender: "Luis Easton",
+    content:
+      "I bought a product from your store in November as a Christmas gift for a member of my family. However it turns out they have something very similar already. I was hoping you could help me with a refund as it is unopened.",
+    time: "25m",
+    color: "bg-indigo-400",
+  },
+  {
+    id: 2,
+    sender: "You",
+    content: "Let me just look into this for you, Luis.",
+    time: "1m",
+    color: "bg-gray-400",
+  },
+];
+
+export { messages, chat };
