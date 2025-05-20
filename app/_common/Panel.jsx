4e738d407b7805174c84dc1f34fa0114d8400dd9
Pratik Kuril <pratikkuril16@gmail.com>
2025-05-20 06:24:52 +0530

    Add custom tabs in panel

--- app/_common/Panel.jsx
+++ app/_common/Panel.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Store } from "lucide-react";
 
 const Panel = () => {
-  return <div className="h-screen flex-2">Panel</div>;
+  return (
+    <div className="h-screen flex-2">
+      <Tabs defaultValue="aicopilot" className="">
+        <TabsList>
+          <TabsTrigger value="aicopilot" className="text-md">
+            {/* <Store /> */}
+            AI Copilot
+          </TabsTrigger>
+          <TabsTrigger value="details" className="text-md">
+            Details
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="aicopilot">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="details">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Panel;
