890028524231fbfb100a988edc1364cdbcaefece
Pratik Kuril <pratikkuril16@gmail.com>
2025-05-24 04:36:10 +0530

    Update SuperCommand content

--- components/SuperCommand.jsx
+++ components/SuperCommand.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 import {
-  Calculator,
-  Calendar,
-  CreditCard,
-  Settings,
+  Bookmark,
   Smile,
-  User,
+  PencilLine,
+  DownloadIcon,
+  Paperclip,
+  Star,
+  ImagePlay,
+  ArrowUp,
+  ArrowDown,
+  CornerDownLeft,
+  Undo2,
 } from "lucide-react";
 
 import {
@@ -20,6 +25,9 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from "@/components/ui/command";
+
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
 
 export function SuperCommand() {
   const [open, setOpen] = React.useState(false);
@@ -37,26 +45,56 @@ export function SuperCommand() {
 
   return (
     <CommandDialog open={open} onOpenChange={setOpen}>
-      <Command className="rounded-lg border shadow-md md:min-w-[450px]">
+      <Command className="rounded-lg border shadow-md md:min-h-[400px] md:min-w-[400px]">
         <CommandInput placeholder="Type a command or search..." />
-        <CommandList>
+        <CommandList className="overflow-hidden">
           <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Suggestions">
+          <CommandGroup heading="">
             <CommandItem>
-              <Calendar />
-              <span>Calendar</span>
+              <PencilLine />
+              <span className="font-medium text-gray-500">Write a note</span>
+              <CommandShortcut>N</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Bookmark />
+              <span className="font-medium text-gray-500">Use macro</span>
+              <CommandShortcut>\</CommandShortcut>
             </CommandItem>
             <CommandItem>
               <Smile />
-              <span>Search Emoji</span>
+              <span className="font-medium text-gray-500">
+                Summarize conversation
+              </span>
+              <CommandShortcut>⌘ Y</CommandShortcut>
             </CommandItem>
-            <CommandItem disabled>
-              <Calculator />
-              <span>Calculator</span>
+            <CommandItem>
+              <DownloadIcon />
+              <span className="font-medium text-gray-500">
+                Create a back-office ticket
+              </span>
+              <CommandShortcut>z</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Star />
+              <span className="font-medium text-gray-500">Snooze</span>
+              <CommandShortcut>⌘ A</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Paperclip />
+              <span className="font-medium text-gray-500">
+                Upload attachment
+              </span>
+              <CommandShortcut>⌘ C</CommandShortcut>
+            </CommandItem>
+            <CommandItem className="pb-2">
+              <ImagePlay />
+              <span className="font-medium text-gray-500">Insert gif</span>
+              <CommandShortcut>⌘B</CommandShortcut>
             </CommandItem>
           </CommandGroup>
           <CommandSeparator />
-          <CommandGroup heading="Settings">
+
+          {/* <CommandGroup heading="Settings">
             <CommandItem>
               <User />
               <span>Profile</span>
@@ -72,8 +110,26 @@ export function SuperCommand() {
               <span>Settings</span>
               <CommandShortcut>⌘S</CommandShortcut>
             </CommandItem>
-          </CommandGroup>
+          </CommandGroup> */}
         </CommandList>
+        {/* BOTTOM LEGEND  */}
+        <div className="mt-2 flex items-center gap-2 p-4">
+          <Button variant="outline" size="xs">
+            <ArrowUp className="font-xs h-4 w-4 text-gray-400" />
+          </Button>
+          <Button variant="outline" size="xs">
+            <ArrowDown className="font-xs h-4 w-4 text-gray-400" />
+          </Button>
+          <span className="text-xs text-gray-400">to navigate</span>
+          <Button variant="outline" size="xs">
+            <CornerDownLeft className="font-xs h-4 w-4 text-gray-400" />
+          </Button>
+          <span className="text-xs text-gray-400">to select</span>
+          <Button variant="outline" size="xs">
+            <Undo2 className="font-xs h-4 w-4 text-gray-400" />
+          </Button>
+          <span className="text-xs text-gray-400">to close</span>
+        </div>
       </Command>
     </CommandDialog>
   );
